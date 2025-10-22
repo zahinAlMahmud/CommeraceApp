7abf437a4979d0f62698912eb6f2f3b83203ba8b
zahinAlMahmud <zahinmahmud327@gmail.com>
2025-10-22 09:16:43 -0700

    Initial commit

--- src/Components/FilterContext.tsx
+++ src/Components/FilterContext.tsx
@@ -7,9 +7,9 @@ interface FilterContextType {
   selectedCategory: string;
   setSelectedCategory: (category: string) => void;
   minPrice: number | undefined;
-  setMinPrice: (price: number | string) => void;
+  setMinPrice: (price: number | undefined) => void;
   maxPrice: number | undefined;
-  setMaxPrice: (price: number | string) => void;
+  setMaxPrice: (price: number | undefined) => void;
   keyword: string;
   setKeyword: (keyword: string) => void;
 }
